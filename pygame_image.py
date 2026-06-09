10e1394810ae0927a2aaef13e54051564038879c
c0a25104f2 <c0a25104f2@edu.teu.ac.jp>
2026-06-09 15:43:09 +0900

    練習５と６：画像遷移と早さの変更

--- pygame_image.py
+++ pygame_image.py
@@ -11,17 +11,18 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kouka_img = pg.image.load("fig/3.png") #練習3こうかとんsurfaceの作成
-    kouka_img = pg.transform.flip(kouka_img,True,False)
+    kouka_img = pg.transform.flip(kouka_img,True,False) #練習4 左右反転
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr
+        screen.blit(bg_img, [-x, 0])#練習5画像を移動
         screen.blit(kouka_img, [300, 200]) #練習4　こうかとんsurfaceを描画
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)#練習6 FPSを変更
 
 
 if __name__ == "__main__":
